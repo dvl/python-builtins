c2cb6d2c33beb485088acc17b59bb0fcc460b877
André Luiz <contato@xdvl.info>
2013-09-26 11:16:28 -0300

    Pep8 compatible

--- builtins.py
+++ builtins.py
@@ -8,17 +8,19 @@ from random import choice
 
 app = Flask(__name__)
 
-choices = ['abs','divmod','input','open','staticmethod','all','enumerate',
-           'int','ord','str','any','eval','isinstance','pow','sum',
-           'basestring','execfile','issubclass','print','super','bin','file',
-           'iter','property','tuple','bool','filter','len','range','type',
-           'bytearray','float','list','raw_input','unichr','callable',
-           'format','locals','reduce','unicode','chr','frozenset','long',
-           'reload','vars','classmethod','getattr','map','repr','xrange',
-           'cmp','globals','max','reversed','zip','compile','hasattr',
-           'memoryview','round','__import__','complex','hash','min','set',
-           'apply','delattr','help','next','setattr','buffer','dict','hex',
-           'object','slice','coerce','dir','id','oct','sorted','intern']
+choices = [
+    'abs', 'divmod', 'input', 'open', 'staticmethod', 'all', 'enumerate',
+    'int', 'ord', 'str', 'any', 'eval', 'isinstance', 'pow', 'sum',
+    'basestring', 'execfile', 'issubclass', 'print', 'super', 'bin', 'file',
+    'iter', 'property', 'tuple', 'bool', 'filter', 'len', 'range', 'type',
+    'bytearray', 'float', 'list', 'raw_input', 'unichr', 'callable',
+    'format', 'locals', 'reduce', 'unicode', 'chr', 'frozenset', 'long',
+    'reload', 'vars', 'classmethod', 'getattr', 'map', 'repr', 'xrange',
+    'cmp', 'globals', 'max', 'reversed', 'zip', 'compile', 'hasattr',
+    'memoryview', 'round', '__import__', 'complex', 'hash', 'min', 'set',
+    'apply', 'delattr', 'help', 'next', 'setattr', 'buffer', 'dict', 'hex',
+    'object', 'slice', 'coerce', 'dir', 'id', 'oct', 'sorted', 'intern'
+]
 
 
 @app.route("/")
@@ -36,8 +38,8 @@ def builtin(builtin=None):
     if builtin not in choices:
         return make_response(render_template('404.html'), 404)
 
-    return render_template('show.html', builtin=builtin, choices=choices,
-                            help=eval(builtin).__doc__)
+    return render_template('show.html', builtin=builtin,
+                           choices=sorted(choices), help=eval(builtin).__doc__)
 
 
 if __name__ == '__main__':
