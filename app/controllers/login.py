7ffeb77e7eeeafca64b4a578cffd84165446fef3
André Luiz <contato@xdvl.info>
2013-10-13 19:48:50 -0300

    adicionado comentarios no controller de login

--- app/controllers/login.py
+++ app/controllers/login.py
@@ -5,6 +5,7 @@ from flask import redirect, url_for, session, request, flash
 from app import github, db
 from app.models.user import User
 
+
 def login():
     return github.authorize(callback=url_for('authorized', _external=True))
 
@@ -25,19 +26,30 @@ def authorized(response):
         flash(u'Houve uma falha na autenticação', 'error')
         return redirect(url_for('index'))
 
+    ''' O maldito Flask-OAuth depende dessa bosta de session e logo está bosta
+    deve existir antes de qualquer requisição que se faça a API do Github. '''
     session['oauth_token'] = (gh_token, '')
 
-    gh_data = github.get('/user')
+    ''' Agora fazemos a maldita requisição '''
+    gh_data = github.get('user')
     gh_id, gh_user = (gh_data.data['id'], gh_data.data['login'])
 
-    user = User.by_username(gh_user)
+    ''' Token: Pode ser alterado revogando acesso a aplicação e depois dando
+    novamente.
+
+    Username: Pode ser alterado facilmente no perfil, embora o proprio Github
+    cite que possa haver efeitos colaterais
+
+    ID: Parece o melhor metodo de busca até o momento '''
+    user = User.by_github_id(gh_id)
 
     if user is None:
         user = User(gh_user, gh_id, gh_token)
         db.session.add(user)
 
+    ''' Levando em conta o que foi dito acima, garantimos que temos os dados do
+    usuario sempre atualizados no banco '''
     user.username = gh_user
-    user.github_id = gh_id
     user.github_access_token = gh_token
 
     db.session.commit()
