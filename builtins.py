ef38ed20c5cfc69c7b08de8df4f9ec1945daf3a4
Elysson MR <elyssonmr@gmail.com>
2013-10-13 14:32:01 -0300

    Implemented REST api

--- builtins.py
+++ builtins.py
@@ -9,6 +9,9 @@ from random import choice
 
 from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask import request
+from flask import make_response
 
 # from pygments import highlight
 # from pygments.lexers import PythonLexer
@@ -36,6 +39,28 @@ def builtin(builtin=None):
     return render_template('show.html', builtin=builtin, choices=choices,
                            help=help_text)
 
+@app.route('/api/v1.0/builtins', methods=['GET'])
+def get_builtins():
+    resp = []
+    for builtin in choices:
+        help_text = getattr(builtins, builtin).__doc__
+        resp.append({'name': builtin, 'help_text':help_text})
+    return jsonify({'builtins': resp})
+
+@app.route('/api/v1.0/builtins/<builtin>', methods=['GET'])
+def get_builtin(builtin=None):
+    if builtin is None or builtin not in choices:
+        return jsonify({'error', 'Resource not found'})
+    help_text = getattr(builtins, builtin).__doc__
+    resp = {'name': builtin, 'help_text': help_text}
+    return jsonify({'builtin': resp})
+
+@app.errorhandler(404)
+def not_found(error):
+    # REST Errors
+    if 'api/v1.0' in request.url:
+        return make_response(jsonify( { 'error': 'Not found' } ), 404)
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
